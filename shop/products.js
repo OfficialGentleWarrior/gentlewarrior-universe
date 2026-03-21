e1d347620488a24422573e5a62e189c3866a5e6f
OfficialGentleWarrior <realgentlewarrior@gmail.com>
2026-03-21 18:50:50 +0800

    Update products.js

--- shop/products.js
+++ shop/products.js
@@ -1,10 +1,10 @@
 const products = [
+
   {
     name: "Young Pork Tocino",
     image: "images/tocino.jpg",
-    description: "Sweet and savory tocino",
+    description: "Sweet and salty",
     category: "Frozen Meat",
-
     variants: [
       { label: "150g", price: 64 },
       { label: "225g", price: 76 },
@@ -12,5 +12,61 @@ const products = [
       { label: "1kg", price: 324 },
       { label: "5kg", price: 1600 }
     ]
+  },
+
+  {
+    name: "Young Pork Tocino FATLESS",
+    image: "images/fatless.jpg",
+    description: "Sweet and salty without the fat!",
+    category: "Frozen Meat",
+    variants: [
+      { label: "225g", price: 85 }
+    ]
+  },
+
+  {
+    name: "Young Pork Tocino CHILI",
+    image: "images/chili.jpg",
+    description: "Sweet, salty and a dash of chili",
+    category: "Frozen Meat",
+    variants: [
+      { label: "225g", price: 85 }
+    ]
+  },
+
+  {
+    name: "Young Pork Barbecue",
+    image: "images/tocino.jpg",
+    description: "Sweet and savory barbecue flavor",
+    category: "Frozen Meat",
+    variants: [
+      { label: "225g", price: 85 },
+      { label: "450g", price: 167 },
+      { label: "1kg", price: 361 }
+    ]
+  },
+
+  {
+    name: "Pork Longganisa",
+    image: "images/longganisa.jpg",
+    description: "Deliciously tender and seasoned with special spices!",
+    category: "Frozen Meat",
+    variants: [
+      { label: "240g", price: 87 },
+      { label: "440g", price: 153 },
+      { label: "1kg", price: 336 }
+    ]
+  },
+
+  {
+    name: "CDO Idol Cheesedog (JUMBO)",
+    image: "images/longganisa.jpg",
+    description: "Delightfully creamy cheese bits in every bite.",
+    category: "Frozen Meat",
+    variants: [
+      { label: "500g", price: 106 },
+      { label: "1kg", price: 175 }
+    ]
   }
+
 ];
