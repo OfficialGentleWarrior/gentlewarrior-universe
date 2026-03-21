cbb3c6d9834367324436f99e70c9b0666e98b73d
OfficialGentleWarrior <realgentlewarrior@gmail.com>
2026-03-21 15:53:57 +0800

    Update products.js

--- shop/products.js
+++ shop/products.js
@@ -1,6 +1,14 @@
 const products = [
-  { name: "Bacon", price: 120, image: "bacon.jpg" },
-  { name: "Hotdog", price: 180, image: "hotdog.jpg" },
-  { name: "Nuggets", price: 150, image: "nuggets.jpg" },
-  { name: "Burger Patties", price: 140, image: "burger.jpg" }
+  {
+    name: "Bacon",
+    price: 120,
+    image: "bacon.jpg",
+    description: "Premium breakfast bacon"
+  },
+  {
+    name: "Hotdog",
+    price: 180,
+    image: "hotdog.jpg",
+    description: "Juicy hotdog, perfect for kids"
+  }
 ];
