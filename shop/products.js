3ec71b1828bdeedb292a361ab04b39eb9b6990ad
OfficialGentleWarrior <realgentlewarrior@gmail.com>
2026-03-21 15:36:19 +0800

    Create products.js

---
+++ shop/products.js
@@ -0,0 +1,6 @@
+const products = [
+  { name: "Bacon", price: 120, image: "bacon.jpg" },
+  { name: "Hotdog", price: 180, image: "hotdog.jpg" },
+  { name: "Nuggets", price: 150, image: "nuggets.jpg" },
+  { name: "Burger Patties", price: 140, image: "burger.jpg" }
+];
