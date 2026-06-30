8b3a983b8ecdfb77cfef95171143e64f0dd9ac6c
OfficialGentleWarrior <realgentlewarrior@gmail.com>
2026-06-30 11:56:13 +0800

    Update brands.js

--- deals/brands.js
+++ deals/brands.js
@@ -1,1 +1,117 @@
+const brands = {
 
+shopping: [
+
+{
+name: "Shopee",
+description: "Online Marketplace",
+image: "assets/brands/shopee.png",
+link: "https://s.shopee.ph/4Vap9a9SfI"
+},
+
+{
+name: "Lazada",
+description: "Online Marketplace",
+image: "assets/brands/lazada.png",
+link: "https://invl.me/clnldh4"
+},
+
+{
+name: "TikTok Shop",
+description: "Social Commerce",
+image: "assets/brands/tiktok.png",
+link: "https://invl.us/clnldhi"
+},
+
+{
+name: "Alibaba",
+description: "Global Wholesale Marketplace",
+image: "assets/brands/alibaba.png",
+link: "https://invl.me/clnldhm"
+},
+
+{
+name: "AliExpress",
+description: "Global Online Marketplace",
+image: "assets/brands/aliexpress.png",
+link: "https://invl.me/clnldhp"
+},
+
+{
+name: "Zalora",
+description: "Online Shopping Platform",
+image: "assets/brands/zalora.png",
+link: "https://invl.me/clnldht"
+},
+
+{
+name: "SHEIN",
+description: "Online Shopping Platform",
+image: "assets/brands/shein.png",
+link: "https://miniurl.app/clnldhw"
+}
+
+],
+
+fashion: [
+
+{
+name: "Puma",
+description: "Sportswear & Footwear",
+image: "assets/brands/puma.png",
+link: "https://invl.me/clnldhz"
+},
+
+{
+name: "Havaianas",
+description: "Footwear Brand",
+image: "assets/brands/havaianas.png",
+link: "https://invl.me/clnldi2"
+},
+
+{
+name: "Charles & Keith",
+description: "Fashion & Accessories",
+image: "assets/brands/charles-keith.png",
+link: "https://invl.me/clnldi3"
+},
+
+{
+name: "Juice Store",
+description: "Fashion Retailer",
+image: "assets/brands/juice-store.png",
+link: "https://invl.me/clnldi6"
+}
+
+],
+
+travel: [
+
+{
+name: "Klook",
+description: "Travel Experiences",
+image: "assets/brands/klook.png",
+link: "https://invl.me/clnldib"
+},
+
+{
+name: "Traveloka",
+description: "Flights & Hotels",
+image: "assets/brands/traveloka.png",
+link: "https://invl.me/clnldi7"
+}
+
+],
+
+finance: [
+
+{
+name: "Home Credit",
+description: "Financial Services",
+image: "assets/brands/homecredit.png",
+link: "#"
+}
+
+]
+
+};
