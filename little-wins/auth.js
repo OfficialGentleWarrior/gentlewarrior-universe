a0308b42395af964912f5faa2a962f731ad571e0
OfficialGentleWarrior <realgentlewarrior@gmail.com>
2026-07-20 20:30:55 +0800

    Update auth.js

--- little-wins/auth.js
+++ little-wins/auth.js
@@ -1,1 +1,55 @@
+// auth.js
 
+import { auth, db } from "./firebase.js";
+
+import {
+  GoogleAuthProvider,
+  signInWithPopup
+} from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+
+import {
+  doc,
+  getDoc,
+  setDoc,
+  serverTimestamp
+} from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
+
+const provider = new GoogleAuthProvider();
+
+export async function signInWithGoogle() {
+
+  try {
+
+    const result = await signInWithPopup(auth, provider);
+
+    const user = result.user;
+
+    const userRef = doc(db, "lw_users", user.uid);
+
+    const snapshot = await getDoc(userRef);
+
+    if (!snapshot.exists()) {
+
+      await setDoc(userRef, {
+
+        uid: user.uid,
+        name: user.displayName,
+        email: user.email,
+        photoURL: user.photoURL,
+
+        createdAt: serverTimestamp()
+
+      });
+
+    }
+
+    window.location.href = "app.html";
+
+  } catch (error) {
+
+    console.error(error);
+    alert(error.message);
+
+  }
+
+}
