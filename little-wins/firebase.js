80ff4de5246256f77238d527e7c13fb2eff8e38a
OfficialGentleWarrior <realgentlewarrior@gmail.com>
2026-07-20 20:29:38 +0800

    Update firebase.js

--- little-wins/firebase.js
+++ little-wins/firebase.js
@@ -1,1 +1,30 @@
+// firebase.js
 
+import { initializeApp, getApps, getApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
+
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDZQFN0iMQ1QvgY2ptRFuOKY1sTz9zZhb8",
+  authDomain: "gentle-warrior.firebaseapp.com",
+  projectId: "gentle-warrior",
+  storageBucket: "gentle-warrior.firebasestorage.app",
+  messagingSenderId: "206508766648",
+  appId: "1:206508766648:web:3451e7dcd6c9008e9c8b4d"
+};
+
+// Prevent duplicate initialization
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+
+// Services
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+// Export
+export { app, db, auth };
