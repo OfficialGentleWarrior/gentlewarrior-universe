792282bc9966009a957b381f2ebe836b274fecfe
OfficialGentleWarrior <realgentlewarrior@gmail.com>
2026-07-28 12:55:31 +0800

    Update redirect.js

--- forge/l/redirect.js
+++ forge/l/redirect.js
@@ -1,1 +1,73 @@
+// ======================================
+// FORGE Redirect Engine
+// redirect.js
+// ======================================
 
+import { db } from "../js/firebase.js";
+
+import {
+    doc,
+    getDoc,
+    updateDoc,
+    increment
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+const statusText = document.getElementById("statusText");
+
+async function redirectLink() {
+
+    // Loading
+    statusText.textContent = "Preparing your destination...";
+
+    // Read short code
+    const params = new URLSearchParams(window.location.search);
+    const code = params.get("code");
+
+    if (!code) {
+        statusText.textContent = "Invalid link. Missing short code.";
+        return;
+    }
+
+    try {
+
+        // Get Firestore document
+        const linkRef = doc(db, "links", code);
+        const linkSnap = await getDoc(linkRef);
+
+        if (!linkSnap.exists()) {
+            statusText.textContent = "This link does not exist.";
+            return;
+        }
+
+        const link = linkSnap.data();
+
+        // Check active status
+        if (!link.isActive) {
+            statusText.textContent = "This link has been disabled.";
+            return;
+        }
+
+        // Increase click counter
+        await updateDoc(linkRef, {
+            clicks: increment(1)
+        });
+
+        statusText.textContent = "Redirecting...";
+
+        // Small delay for smoother UX
+        setTimeout(() => {
+            window.location.replace(link.destinationUrl);
+        }, 300);
+
+    } catch (error) {
+
+        console.error("Redirect Error:", error);
+
+        statusText.textContent =
+            "Unable to redirect. Please try again later.";
+
+    }
+
+}
+
+redirectLink();
