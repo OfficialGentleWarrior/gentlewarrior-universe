e930821576689ecc43449b83cf774733df17a9b7
OfficialGentleWarrior <realgentlewarrior@gmail.com>
2026-08-11 09:44:08 +0800

    Create facebook-tasks.js

---
+++ forge/js/facebook-tasks.js
@@ -0,0 +1,20 @@
+/* ======================================
+   FORGE Facebook Tasks
+   Isolated Facebook Module
+====================================== */
+
+const FACEBOOK_TASKS = {
+
+    follow_baby_leam: {
+        title: "Follow Baby Leam's Page",
+        link: "https://www.facebook.com/BabyLeamOfficial"
+    },
+
+    follow_gentle_warrior: {
+        title: "Follow Gentle Warrior's Page",
+        link: "https://www.facebook.com/GentleWarriorOfficial"
+    }
+
+};
+
+console.log("Facebook Task Module Loaded");
